a044f94abc70a04369293a7f67a4e9a544d54fc9
Raluca Ciucu <raluca.ciucu@gmail.com>
2019-12-03 20:27:12 +0000

    Add comments

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -2,8 +2,11 @@ require 'pg'
 
 class Bookmark
   def self.all 
+    #using pg to connect to database
     connection = PG.connect(dbname: 'bookmark_manager')
+    #running a query on the connection previously established
     result = connection.exec("SELECT * FROM bookmarks;")
+    #assign that to a result variable and map over it to return the urls:
     result.map { |bookmark| bookmark['url'] }
    end 
   end
