04cafc75ca7bdacc0999a346a605e02f70e2f438
Raluca Ciucu <raluca.ciucu@gmail.com>
2019-12-06 14:32:13 +0000

    Add test to check if Bookmark object behaves as expected and if db stores data as expected

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,9 +1,10 @@
 require 'pg'
 
 class Bookmark
-  attr_reader :url, :title
+  attr_reader :id, :url, :title
 
-  def initialize (url:, title:)
+  def initialize (id:, url:, title:)
+    @id = id
     @url = url
     @title = title
   end
@@ -21,11 +22,11 @@ class Bookmark
     #running a query on the connection previously established
     result = connection.exec("SELECT * FROM bookmarks;")
     #assign that to a result variable and map over it to return the details:
-    #it wraps up a row in the bookmarks table in an array object. the whole table becomes an array of arrays.
-    result.map { |bookmark| Bookmark.create(url: bookmark['url'], title: bookmark['title']) }
+    #it wraps up a row in the bookmarks table in an arrhashhashay object. the whole table becomes an array of hashes.
+    result.map { |bookmark| Bookmark.new(id: bookmark ['id'], url: bookmark['url'], title: bookmark['title']) }
   end 
 
-
+  #We're already returning the new Bookmark when it's created, so let's wrap this in a Bookmark object:
   def self.create(url:, title:)
   
     if ENV['ENVIRONMENT'] == 'test'
@@ -34,7 +35,7 @@ class Bookmark
       connection = PG.connect(dbname: 'bookmark_manager')
     end
 
-    result = connection.exec("INSERT INTO bookmarks (url, title) VALUES('#{url}','#{title}') RETURNING url, title")
-    Bookmark.new(url: result[0]['url'], title: result[0]['title'])
+    result = connection.exec("INSERT INTO bookmarks (url, title) VALUES('#{url}','#{title}') RETURNING id,  url, title")
+    Bookmark.new(id: result[0]['id'], url: result[0]['url'], title: result[0]['title'])
   end
 end
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -1,4 +1,5 @@
 require 'bookmark'
+require 'database_helpers'
 
 describe Bookmark do
   describe '.all' do
@@ -6,24 +7,41 @@ describe Bookmark do
       connection = PG.connect(dbname: 'bookmark_manager_test')
 
       # Add the test data
-      connection.exec("INSERT INTO bookmarks (url, title) VALUES ('http://www.makersacademy.com', 'Makers');")
-      connection.exec("INSERT INTO bookmarks (url, title) VALUES('http://www.destroyallsoftware.com', 'Destroy All Software');")
-      connection.exec("INSERT INTO bookmarks (url, title) VALUES('http://www.google.com', 'Google');")
+
+      bookmark = Bookmark.create(url: "http://www.makersacademy.com", title: "Makers Academy")
+      Bookmark.create(url: "http://www.destroyallsoftware.com", title: "Destroy All Software")
+      Bookmark.create(url: "http://www.google.com", title: "Google")
+      #former test data
+      # connection.exec("INSERT INTO bookmarks (url, title) VALUES ('http://www.makersacademy.com', 'Makers');")
+      # connection.exec("INSERT INTO bookmarks (url, title) VALUES('http://www.destroyallsoftware.com', 'Destroy All Software');")
+      # connection.exec("INSERT INTO bookmarks (url, title) VALUES('http://www.google.com', 'Google');")
 
       bookmarks = Bookmark.all
 
-      expect(bookmarks[0].url).to include('http://www.makersacademy.com')
+      #this tests that the Bookmark objects returns the same data we input into the database, 
+      #basically, that our object behaves the way we want it to
+      expect(bookmarks.length).to eq 3
+      expect(bookmarks.first).to be_a Bookmark
+      expect(bookmarks.first.id).to eq bookmark.id
+      expect(bookmarks.first.title).to eq 'Makers Academy'
+      expect(bookmarks.first.url).to eq 'http://www.makersacademy.com'
       end
   end
 
   describe ".create" do
-    it "creates a new bookmark entry" do
-      Bookmark.create(url: "https://www.bbcgoodfood.com/", title: "BBC Good Food")
 
-      expect(Bookmark.all[-1].url).to eq 'https://www.bbcgoodfood.com/'
-      expect(Bookmark.all[-1].title).to eq 'BBC Good Food'
-      p Bookmark.all
+    #this tests that the data we input for the database 
+    #ends up in the correct database, the way we want it
+    it "creates a new bookmark entry" do
+      bookmark = Bookmark.create(url: "https://www.bbcgoodfood.com/", title: "BBC Good Food")
+      persisted_data = persisted_data(id: bookmark.id)      
+
+      expect(bookmark).to be_a Bookmark
+      expect(bookmark.id).to eq persisted_data['id']
+      expect(bookmark.url).to eq 'https://www.bbcgoodfood.com/'
+      expect(bookmark.title).to eq 'BBC Good Food'
     end
 
   end
+
 end
